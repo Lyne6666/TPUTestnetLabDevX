8f265ad1ee417ffc4dea8d93feb22b8d83c9316e
UHSR <uhsr@eteb.me>
2025-10-08 23:06:45 +0700

    Enhance lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,28 @@
  */
 
 use log::{info, error};
+use env_logger::{Builder, init};
 
+/// Custom result type for the library
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+/// Initialize the logger based on verbosity level
+fn init_logger(verbose: bool) -> Result<()> {
+    if verbose {
+        Builder::from_default_env()
+            .filter_level(log::LevelFilter::Debug)
+            .init()?;
+    } else {
+        init()?;
+    }
+    Ok(())
+}
 
 /// Main processing function
 pub fn run(verbose: bool) -> Result<()> {
-    if verbose {
-        env_logger::Builder::from_default_env()
-            .filter_level(log::LevelFilter::Debug)
-            .init();
-    } else {
-        env_logger::init();
-    }
-    
+    // Initialize the logger
+    init_logger(verbose)?;
+
     info!("Starting TPUTestnetLabDevX processing");
     
     // Add your implementation here
